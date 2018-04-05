77e29b1f975712f19c6b793e2ccfc2dd49898774
Dmitriy Sharshakov aka. sh7dm <sh7dm@outlook.com>
2018-04-05 21:29:19 +0300

    Added task get and update functions 📥 📤

--- src/router/api/tasks.js
+++ src/router/api/tasks.js
@@ -9,6 +9,7 @@ router.get('/', async ctx => {
   try {
     ctx.body = { tasks: await Task.find(), code: 200 }
   } catch (err) {
+    console.error(err)
     ctx.body = { code: 500, err: err }
   }
 })
@@ -52,6 +53,66 @@ router.post('/', async ctx => {
       ctx.body = { code: 401, err: err }
       return
     }
+
+    console.error(err)
+    ctx.body = { code: 500, err: err }
+  }
+})
+
+router.get('/:id', async ctx => {
+
+  try {
+    let foundTask = await Task.findOne({ id: ctx.params.id })
+
+    if (!foundTask) {
+      ctx.body = { code: 404 }
+      return
+    }
+
+    ctx.body = { task: foundTask, code: 200 }
+  } catch (err) {
+    console.error(err)
+    ctx.body = { code: 500, err: err }
+  }
+})
+router.patch('/:id', async ctx => {
+  if (!ctx.request.body.token || !ctx.request.body.name || !ctx.request.body.flag || !ctx.request.body.money || !ctx.request.body.experience || !ctx.request.body.url) {
+    ctx.body = { code: 400 }
+    return
+  }
+
+  try {
+    let payload = jwt.verify(ctx.request.body.token, jwtConfig.secret)
+
+    if (payload.role != 'admin') {
+      ctx.body = { code: 401 }
+      return
+    }
+
+    let foundTask = await Task.findOne({ id: ctx.params.id })
+
+    if (!foundTask) {
+      ctx.body = { code: 404 }
+      return
+    }
+
+    foundTask.name = ctx.request.body.name
+    foundTask.flag = ctx.request.body.flag,
+    foundTask.added = Date(),
+    foundTask.money = ctx.request.body.money,
+    foundTask.experience = ctx.request.body.experience,
+    foundTask.url = ctx.request.body.url
+
+    await foundTask.save()
+
+    ctx.body = { task: foundTask, code: 200 }
+  } catch (err) {
+    if (err.name && err.name === 'TokenExpiredError') {
+      ctx.body = { code: 401, err: err }
+      return
+    }
+
+    console.error(err)
     ctx.body = { code: 500, err: err }
   }
 })
