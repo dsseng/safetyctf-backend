3e25d42c92f8ae51d0e5658e6a4ae50e8165e1a4
Dmitriy Sharshakov aka. sh7dm <sh7dm@outlook.com>
2018-04-06 07:08:44 +0300

    Changed user info request

--- src/router/api/auth.js
+++ src/router/api/auth.js
@@ -131,29 +131,11 @@ router.post('/changePassword', async ctx => {
   }
 })
 
-router.post('/getUserInfo', ctx => {
-  if (!ctx.request.body.token) {
-    ctx.body = { code: 400 }
-    return
-  }
-
+router.get('/:username/info', async ctx => {
   try {
-    let decoded = jwt.verify(ctx.request.body.token, jwtConfig.secret)
-
-    ctx.body = { user: {
-      username: decoded.username,
-      name: decoded.name,
-      surname: decoded.surname,
-      dob: decoded.dob,
-      role: decoded.role,
-      tasksSolved: decoded.tasksSolved,
-      money: decoded.money,
-      experience: decoded.experience,
-      registerDate: decoded.registerDate,
-      friends: decoded.friends
-    }, code: 200 }
+    ctx.body = { user: await User.findOne({ username: ctx.params.username }), code: 200 }
   } catch (err) {
-    ctx.body = { code: 401, err: err }
+    ctx.body = { code: 500, err: err }
     return
   }
 })
