64b911de0d103767a3f5258e73208e266dea1958
sh7dm <sh7dm@outlook.com>
2018-05-24 15:36:45 +0300

    🏆 Added leaderboard

--- src/router/api/stats.js
+++ src/router/api/stats.js
@@ -17,4 +17,29 @@ router.get('/usersAndTasks', async ctx => {
   }
 })
 
+router.get('/leaderboard', async ctx => {
+  try {
+    let users = (await User.find({}))
+    let i, j
+
+    for (i = 0; i < users.length; i++) { // Sort users by experience
+      let value = users[i]
+      // store the current item value so it can be placed right
+      for (j = i - 1; j > -1 && users[j].experience < value.experience; j--) {
+        // loop through the items in the sorted array (the items from the current to the beginning)
+        // copy each item to the next one
+        users[j + 1] = users[j]
+      }
+      // the last item we've reached should now hold the value of the currently sorted item
+      users[j + 1] = value
+    }
+
+    ctx.body = { users: users, code: 200 }
+  } catch (err) {
+    console.error(err)
+    ctx.body = { code: 500, err: err }
+    return
+  }
+})
+
 export default router
