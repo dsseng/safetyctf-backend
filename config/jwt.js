319243e8f276acb27de4dcb6c54de9020b895402
sh7dm <sh7dm@outlook.com>
2018-07-21 16:58:39 +0300

    🔐 Moved JWT secret to the .env

--- config/jwt.js
+++ config/jwt.js
@@ -1,5 +1,10 @@
+import dotenv from 'dotenv'
+import debug from 'debug'
+
+dotenv.config()
+
 export default {
-  secret: 'shared secret',
+  secret: process.env.JWT_SECRET,
   options: {
     expiresIn: '1h'
   }
